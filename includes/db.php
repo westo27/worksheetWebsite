a5499130dbacb5ac131be5942414eeffe4148803
westo27 <west1_15@uni.worc.ac.uk>
2019-04-04 14:38:37 +0100

    changed login details

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'todo';
-$user = 'root';
-$pass = '';
+//$host = 'localhost';
+//$dbname = 'todo';
+//$user = 'root';
+//$pass = '';
 
-//$host = '172.16.11.22:3306';
-//$dbname = 'west1_15_todo';
-//$user = 'west1_todo';
-//$pass = 'Dog123';
+$host = '172.16.11.22:3306';
+$dbname = 'west1_15_todo';
+$user = 'west1_todo';
+$pass = 'Dog123';
 
 try 
 {
